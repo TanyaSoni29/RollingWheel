20ca2931415663d579600a49ebe9db2ea760299b
TanyaSoni2912 <tanu139sona@gmail.com>
2024-07-24 17:49:41 +0530

    category card and animation

--- src/components/Core/Home/CategoryCard.jsx
+++ src/components/Core/Home/CategoryCard.jsx
@@ -1,24 +1,61 @@
+// /** @format */
+
+// import React from 'react';
+
+
+// function CategoryCard({image, title}) {
+// 	return (
+// 		<div className='w-11/12 h-full flex flex-col justify-center items-center '>
+// 			<div className='relative z-[90] flex flex-col justify-center items-center gap-6 cursor-pointer group'>
+// 				<div className='w-[500px] h-[500px]'>
+// 					<img
+// 						src={image}
+// 						className='w-full h-full object-cover scale-100 duration-700 transition-all group-hover:scale-105'
+// 					/>
+// 				</div>
+// 				<button className='w-[60%] uppercase px-4 py-3 border-cyan-800 border-[1px] group-hover:border-cyan-950 cursor-pointer group-hover:border-t-2 group-hover:border-b-2 group-hover:-translate-y-2 transition-all duration-300'>
+// 					Shop Now
+// 				</button>
+//                 <div className='absolute z-[80] text-center left-6 bottom-20'>
+//                     <p className='text-white font-semibold text-6xl uppercase'>{title}</p>
+//                 </div>
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+// export default CategoryCard;
+
+
 /** @format */
 
 import React from 'react';
-import Image from '../../../assets/Images/categoryCard/category-brand-cube.jpg';
+import { useInView } from 'react-intersection-observer';
+import './CategoryCard.css';
+function CategoryCard({ image, title }) {
+    const { ref, inView } = useInView({
+        triggerOnce: true, 
+    });
 
-function CategoryCard() {
-	return (
-		<div className='w-11/12 h-full flex flex-col justify-center items-center '>
-			<div className='flex flex-col justify-center items-center gap-4 cursor-pointer group'>
-				<div className='w-[500px] h-[500px]'>
-					<img
-						src={Image}
-						className='w-full h-full object-cover'
-					/>
-				</div>
-				<button className='w-[60%] uppercase px-4 py-3 border-cyan-800 border-[1px] group-hover:border-cyan-950 cursor-pointer group-hover:border-t-2'>
-					Shop Now{' '}
-				</button>
-			</div>
-		</div>
-	);
+    return (
+        <div className={`w-11/12 h-full flex flex-col justify-center items-center ${inView ? 'card-visible' : 'card-hidden'}`} ref={ref}>
+            <div className='relative z-[90] flex flex-col justify-center items-center gap-6 cursor-pointer group transition-transform duration-1000'>
+                <div className='w-[500px] h-[500px]'>
+                    <img
+                        src={image}
+                        className='w-full h-full object-cover scale-100 duration-700 transition-all group-hover:scale-105'
+                    />
+                </div>
+                <button className='w-[60%] uppercase px-4 py-3 border-cyan-800 border-[1px] group-hover:border-cyan-950 cursor-pointer group-hover:border-t-2 group-hover:border-b-2 group-hover:-translate-y-2 transition-all duration-300'>
+                    Shop Now
+                </button>
+                <div className='absolute z-[80] text-center left-6 bottom-20'>
+                    <p className='text-white font-semibold text-6xl uppercase'>{title}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default CategoryCard;
+
--- src/components/Core/Home/CategoryCards.jsx
+++ src/components/Core/Home/CategoryCards.jsx
@@ -2,20 +2,58 @@
 
 import React from 'react';
 import CategoryCard from './CategoryCard';
+import BikeImg from '../../../assets/Images/categoryCard/category-electric-road.jpg';
+import HelmetCategoryImg from '../../../assets/Images/categoryCard/category-dept-helmets.jpg';
+import ClothingImg from '../../../assets/Images/categoryCard/category-dept-clothing.jpg';
+import AccessoriesImg from '../../../assets/Images/categoryCard/category-dept-lights.jpg';
+import ElectricBikeImg from '../../../assets/Images/categoryCard/category-electric-mountain.jpg';
+import ComponentImg from "../../../assets/Images/categoryCard/category-dept-bikes.jpg"
 
 function CategoryCards() {
+	const CategoryCardData = [
+		{
+			id: 1,
+			image: BikeImg,
+			title: 'Bikes',
+		},
+		{
+			id: 2,
+			image: ElectricBikeImg,
+			title: 'Electric Bikes',
+		},
+		{
+			id: 3,
+			image: ClothingImg,
+			title: 'Clothing',
+		},
+		{
+			id: 4,
+			image: HelmetCategoryImg,
+			title: 'Helmets',
+		},
+		{
+			id: 5,
+			image: AccessoriesImg,
+			title: 'Accessories',
+		},
+		{
+			id: 6,
+			image: ComponentImg,
+			title: 'Components',
+		},
+	];
 	return (
-        <div className='mt-8 mb-8 w-full h-full flex justify-center items-center gap-6'>
-        <div className='grid grid-cols-3 gap-4 place-content-center'>
-			<CategoryCard />
-			<CategoryCard /> 
-            <CategoryCard /> 
-            <CategoryCard /> 
-            <CategoryCard />
-			<CategoryCard />
+		<div className='mt-8 mb-8 w-full h-full flex justify-center items-center gap-6'>
+			<div className='grid grid-cols-3 gap-4 place-content-center'>
+				{CategoryCardData.map((card) => (
+					<CategoryCard
+						key={card.id}
+						image={card.image}
+						title={card.title}
+					/>
+				))}
+			</div>
 		</div>
-        </div>
-		
 	);
 }
 
