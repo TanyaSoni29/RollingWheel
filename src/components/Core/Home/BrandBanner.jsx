629e821e1c629c681cbb6d49eab0a2179ea09738
TanyaSoni2912 <tanu139sona@gmail.com>
2024-07-27 12:35:55 +0530

    BrandBanner Responsiveness Added

--- src/components/Core/Home/BrandBanner.jsx
+++ src/components/Core/Home/BrandBanner.jsx
@@ -9,8 +9,8 @@ function BrandBanner({ brand, brandDescription }) {
 	const { ref: cycleImageRef, inView: isCycleImageIsVisible } = useInView();
 	return (
 		<div className='w-full flex justify-center items-center p-2 bg-gray-100 hover:bg-[#D2D2D2] transition-all duration-300 cursor-pointer mt-6'>
-			<div className=' w-11/12 h-full flex justify-evenly items-center px-4'>
-				<div className='w-[20%] h-[20%]'>
+			<div className='w-full md:w-11/12 h-full flex justify-evenly items-center px-0 md:px-4'>
+				<div className='w-[30%] h-[30%] md:w-[35%] md:h-[35%] xl:w-[20%] xl:h-[20%]'>
 					<img
 						src={TrekBrandImg}
 						alt='Trek Brand'
@@ -23,15 +23,15 @@ function BrandBanner({ brand, brandDescription }) {
 						ref={cycleImageRef}
 					/>
 				</div>
-				<div className=' w-[25%] flex flex-col justify-center items-center gap-4'>
-					<div className='border-black border-[2px] text-3xl font-bold py-4 px-6 text-lime-950 text-center'>
+				<div className='w-[40%] md:w-[30%] xl:w-[25%] flex flex-col justify-center items-center gap-4'>
+					<div className='border-black border-[2px] text-[1rem] md:text-xl lg:text-3xl font-bold py-2 sm:py-4 px-4 sm:px-6 text-lime-950 text-center'>
 						{brand}
 					</div>
-					<p className='text-2xl font-semibold text-center'>
+					<p className='text-[0.7rem] md:text-[1rem] lg:text-2xl font-semibold text-center'>
 						{brandDescription}
 					</p>
 				</div>
-				<div className='w-[20%] h-[20%]'>
+				<div className='w-[30%] h-[30%] md:w-[35%] md:h-[35%] xl:w-[20%] xl:h-[20%]'>
 					<img
 						ref={cycleImageRef}
 						src={TrekBrandImg2}
